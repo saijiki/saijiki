9d6e8a05fedd34f49316ece46c9a7e0eb24548b5
malvaceae <malvaceae@hotmail.com>
2019-05-20 22:00:00 +0900

    update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,9 +3,10 @@ module.exports = {
     indexPath: '../resources/views/welcome.blade.php',
     devServer: {
         proxy: {
-            '^/api': {
+            '^/api($|/)': {
                 target: 'http://192.168.99.100',
             },
         },
+        public: '192.168.99.100:8000',
     },
 };
