f4ee00a7e3b05f8d1025b5eb230c78b5522b6964
Marimo <w-kouta@kni.biglobe.ne.jp>
2019-11-09 21:12:06 +0900

    日付と順序、キーワード検索機能の実装

--- app/Http/Controllers/SenryuController.php
+++ app/Http/Controllers/SenryuController.php
@@ -4,18 +4,52 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Senryu;
+use Carbon\Carbon;
 
 class SenryuController extends Controller
 {
     /**
      * 川柳一覧を取得する。
      *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function index(Request $request)
     {
-        $senryus = Senryu::select('id', 'path')
-            ->orderByDesc('created_at')
+        $param = $request->all();
+
+        $builder = Senryu::select('id', 'path');
+
+        // 順序
+        if($request->has('order')) {
+            if($param['order'] == "人気順") {
+                $builder->orderByDesc('good');
+            }
+        }
+
+        // 期間
+        if($request->has('period')) {
+
+            if ($param['period'] == '今日') {
+                $dt = Carbon::today();
+                $senryus = $builder->where('created_at', '>=', $dt);
+            } 
+            else if ($param['period'] == '今週') {
+                $dt = Carbon::now()->subDay(7);
+                $senryus = $builder->where('created_at', '>=', $dt);
+            }
+            else if ($param['period'] == '今月') {
+                $dt = Carbon::now()->subMonday(1);
+                $senryus = $builder->where('created_at', '>=', $dt);
+            }
+        }
+
+        // キーワード検索
+        if($request->has('word')) {
+           $senryus = $builder->where('body', 'like', '%' . $param['word'] . '%');
+        }
+
+        $senryus = $builder->orderByDesc('created_at')
             ->paginate(6);
 
         return response()->json($senryus);
