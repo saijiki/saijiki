245d3397b14d88d66c92ae14ca17240f5bb6f065
malvaceae <malvaceae@hotmail.com>
2019-11-21 21:49:58 +0900

    OGPの設定を修正

--- resources/views/ogp.blade.php
+++ resources/views/ogp.blade.php
@@ -5,9 +5,10 @@
     <title>Saijiki</title>
     <meta name="twitter:card" content="summary">
     <meta property="og:title" content="Saijiki">
-    <meta property="og:type" content="website">
-    <meta property="og:description" content="{{ $senryu->body }}">
+    <meta property="og:type" content="article">
     <meta property="og:url" content="{{ url()->current() }}">
     <meta property="og:image" content="{{ $senryu->path }}">
+    <meta property="og:description" content="{{ $senryu->body }}">
+    <meta property="og:site_name" content="Saijiki">
   </head>
 </html>
