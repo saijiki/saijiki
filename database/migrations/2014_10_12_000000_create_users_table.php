065403a53b8b16b5384d9b5f2c4b8f4009155212
malvaceae <malvaceae@hotmail.com>
2019-12-02 23:20:47 +0900

    ユーザテーブルを修正

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('twitter_id')->nullable()->unique();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('email')->nullable()->unique();
+            $table->string('password')->nullable();
+            $table->string('avatar')->nullable();
             $table->timestamps();
         });
     }
